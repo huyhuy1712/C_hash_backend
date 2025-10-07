cc29de56c569bc1d80fa277ff5ffca3113dc0bb8
huyhuy1712 <anhhuy17122004@gmail.com>
2025-10-07 17:15:08 +0700

    sửa lại database

---
+++ MilkTeaApp/MilkTea.Server/Models/ctdonhang_topping.cs
@@ -0,0 +1,11 @@
+namespace MilkTea.Server.Models
+{
+    public class ctdonhang_topping
+    {
+        public int MaNL { get; set; }
+        public string MaCTDH { get; set; } 
+        public string SL { get; set; } 
+        
+
+    }
+}

b7ad071350d40a7e27171c7fb121b08be9586828
Retsera <3122410309.sv.sgu.edu.vn>
2025-10-11 23:11:13 +0700

    123

--- MilkTeaApp/MilkTea.Server/obj/Debug/net8.0/MilkTea.Server.AssemblyInfo.cs
+++ MilkTeaApp/MilkTea.Server/obj/Debug/net8.0/MilkTea.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MilkTea.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22686595b56b58d6cd54672c335fad26c8546d0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d0b7161303e1d7a3cb4493b44516f5e85420f0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MilkTea.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MilkTea.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
